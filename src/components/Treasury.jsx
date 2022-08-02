51f0861e224a74922ce3ca5f33764f708342a6ae
satyamkhatrani <100205620+satyamkhatrani@users.noreply.github.com>
2022-08-02 18:23:55 +0530

    remove unused code

--- src/components/Treasury.jsx
+++ src/components/Treasury.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAccount } from "wagmi";
 import Asset from "./Asset";
 
 const AssetsData = [
@@ -26,65 +27,6 @@ const AssetsData = [
 ];
 
 const Treasury = () => {
-  const { address } = useAccount();
-  const [treasuryContract, setTreasury] = useState();
-  const [tokenContract, setToken] = useState();
-  const [balance, setBalance] = useState();
-  const [tokenName, setTokenName] = useState();
-  const [tokenSymbol, setTokenSymbol] = useState();
-  const [ethBalance, setEthBalance] = useState();
-
-  const init = async () => {
-    let provider = new ethers.providers.Web3Provider(window.ethereum);
-    let signer = await provider.getSigner();
-    let treasury = new ethers.Contract(
-      contractAddress.NFTDeposit,
-      treasuryABI,
-      signer
-    );
-    let token = new ethers.Contract(
-      contractAddress.TokenAddress,
-      erc20ABI,
-      signer
-    );
-    let TokenBalance = await token.balanceOf(contractAddress.NFTDeposit);
-    let TokenName = await token.name();
-    let TokenSymbol = await token.symbol();
-    let etherBal = await provider.getBalance(contractAddress.NFTDeposit);
-    setTreasury(treasury);
-    setToken(token);
-    setTokenName(TokenName);
-    setTokenSymbol(TokenSymbol);
-    setBalance(ethers.utils.formatEther(TokenBalance));
-    setEthBalance(ethers.utils.formatEther(etherBal));
-  };
-
-  const depositTokenToTreasury = async (amount) => {
-    let allowance = await tokenContract.allowance(
-      address,
-      contractAddress.NFTDeposit
-    );
-    if (ethers.utils.formatEther(allowance) == 0) {
-      let approveTx = await tokenContract.approve(
-        contractAddress.NFTDeposit,
-        "115792089237316195423570985008687907853269984665640564039457584007913129639935"
-      );
-      let depositTx = await treasuryContract.depositToken(amount);
-    } else {
-      let depositTx = await treasuryContract.depositToken(amount);
-    }
-  };
-
-  const depositEth = async () => {
-    let depositTx = await treasuryContract.depositEth("100000000000000000", {
-      value: ethers.utils.parseEther("0.1"),
-    });
-  };
-
-  useEffect(() => {
-    init();
-  }, []);
-
   return (
     <>
       <div>
